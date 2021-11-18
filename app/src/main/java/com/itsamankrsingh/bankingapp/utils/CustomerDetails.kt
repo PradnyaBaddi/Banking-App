720e62829e94bcb93c93b01ed7ae1d560cf12ee5
“Pradnya <pradnyabaddi07@gmail.com>
2021-11-18 21:54:38 +0530

    Banking App

---
+++ app/src/main/java/com/itsamankrsingh/bankingapp/utils/CustomerDetails.kt
@@ -0,0 +1,112 @@
+package com.itsamankrsingh.bankingapp.utils
+
+import com.itsamankrsingh.bankingapp.database.Customer
+
+
+class CustomerDetails {
+
+
+
+    companion object{
+
+        val customer1 = Customer(
+            0,
+            "Pradnya Baddi",
+            "pradnya07@gmail.com",
+            "0123456789",
+            "910123456789",
+            "SBI012345",
+            10000
+        )
+        val customer2 = Customer(
+            0,
+            "Sakshi Dhoni",
+            "sakshidhoni07@gmail.com",
+            "0123456799",
+            "750123456799",
+            "PNB012345",
+            2000000
+        )
+        val customer3 = Customer(
+            0,
+            "Rajkumar Roy",
+            "raj@gmail.com",
+            "0123455789",
+            "640123455789",
+            "ICICI012345",
+            60000
+        )
+        val customer4 = Customer(
+            0,
+            "John Cena",
+            "john@friends.com",
+            "9123456787",
+            "259123456787",
+            "SBI712349",
+            1000
+        )
+        val customer5 = Customer(
+            0,
+            "Chandler",
+            "chandler@friends.com",
+            "6123756589",
+            "006123756589",
+            "HDFC012345",
+            90000
+        )
+        val customer6 = Customer(
+            0,
+            "Ross Geller",
+            "ross@friends.com",
+            "0161456789",
+            "380161456789",
+            "PNB012345",
+            50000
+        )
+        val customer7 = Customer(
+            0,
+            "Rajesh Tiwari",
+            "rajesh@tbbt.com",
+            "3583456789",
+            "543583456789",
+            "SBI54334",
+            90000
+        )
+        val customer8 = Customer(
+            0,
+            "Jack Christian",
+            "hellojack@tbbt.com",
+            "0127756789",
+            "170127756789",
+            "SBI077745",
+            30000
+        )
+        val customer9 = Customer(
+            0,
+            "Tiger Roshan",
+            "officialtiger@tbbt.com",
+            "7231456789",
+            "697231456789",
+            "SBI71231",
+            70000
+        )
+        val customer10 = Customer(
+            0,
+            "Howard Potter",
+            "howard@tbbt.com",
+            "0129046789",
+            "500129046789",
+            "CITI99434",
+            30000
+        )
+        val customer11 = Customer(
+            0,
+        "Vaishnavi Patil",
+        "vaishnavi077@tbbt.com",
+        "9129046789",
+        "829129046789",
+        "CITI99474",
+        80000
+        )
+    }
+}
